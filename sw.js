4001ea8d0865e280e87a5f9c4c66f12b583a083b
Ben Foxall <benfoxall@gmail.com>
2016-06-02 12:20:20 +0300

    Kind of working, but not loading back from cache

--- sw.js
+++ sw.js
@@ -3,8 +3,25 @@ console.log("sw.js (sw-editor)")
 // TODO - precache / + 'sw.js' in PRIMARY
 
 self.addEventListener('message', function(message) {
+  var data = message.data
 
-  // if data.path && data.body - populate the cache
-  console.log("message>" + message)
+  // push content into the cache
+  if(data.path && data.body && data.type){
+    console.log("save> ", data.path, data.type, data.body.length)
+    caches.open('content')
+      .then( cache =>
+        cache.put(data.path, new Response(data.body, {
+          headers: {
+            'Content-Type': data.type
+          }
+        }))
+      )
+  }
 
+})
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(r => r || fetch(event.request))
+  )
 })
