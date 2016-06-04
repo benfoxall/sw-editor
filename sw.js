5e35b0f120329f4ba7b08157463673988f0f9f4b
Ben Foxall <benfoxall@gmail.com>
2016-06-04 19:04:31 +0300

    move cache editing from sw + implement list & delete

--- sw.js
+++ sw.js
@@ -1,24 +1,5 @@
-console.log("sw.js (sw-editor)")
-
 // TODO - precache / + 'sw.js' in PRIMARY
 
-self.addEventListener('message', function(message) {
-  var data = message.data
-
-  // push content into the cache
-  if(data.path && data.body && data.type){
-    console.log("save> ", data.path, data.type, data.body.length)
-    caches.open('content')
-      .then( cache =>
-        cache.put(data.path, new Response(data.body, {
-          headers: {
-            'Content-Type': data.type
-          }
-        }))
-      )
-  }
-
-})
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
