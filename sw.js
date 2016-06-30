a6ecc4b7484955d28014fc2e8a7e07b43e6b4a4f
Ben Foxall <benfoxall@gmail.com>
2016-06-30 09:27:31 +0100

    load initial cache contents

--- sw.js
+++ sw.js
@@ -6,3 +6,25 @@ self.addEventListener('fetch', function(event) {
       .then(r => r || fetch(event.request))
   )
 })
+
+
+// Load default files on install
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    // this could use cache.addAll, but we need to
+    // map the requests
+    caches.open('content').then(cache =>
+      Promise.all([
+        'index.html',
+        'script.js',
+        'style.css'
+      ].map( file =>
+        fetch('/~default/' + file)
+          .then(resp =>
+            cache.put('/' + file, resp)
+          )
+        )
+      )
+    )
+  )
+})
